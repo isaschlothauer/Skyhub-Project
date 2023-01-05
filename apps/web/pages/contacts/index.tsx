773f396752adfb33c0aa3bc9d7c0ddfd332569de
Kukuruzka1 <irinakukuruza295@gmail.com>
2023-01-05 22:09:10 +0100

    adjusted destop version for contact form. adjusted mobile version for GoBackHome component

--- apps/web/pages/contacts/index.tsx
+++ apps/web/pages/contacts/index.tsx
@@ -7,7 +7,7 @@ import contactUsStyle from "./contactus.module.scss";
 import Footer from "../../components/Footer";
 import Mini_Header from "../../components/Header";
 import Button from "../../components/GeneralButton";
-//import FAQTile from "../../components/FAQ_contact_tile"
+import GoHomeContainer from "../../components/GoBackContainer";
 
 const submitButton = {
     route: "/contacts",
@@ -19,21 +19,23 @@ const ContactUs = () => {
     return (
     <div className={`${contactUsStyle["all-container"]} ${contactUsStyle["header-otherpages"]}`}>
      <Mini_Header title={"Contact Us"} />
-      <div className={"mx-auto sm:px-4"}>
-         <div className={"flex flex-wrap"}>
-          <div className={`w-4/5 ${contactUsStyle["contact-form"]}`}>
-            <p className={` pt-4 pb-4 mb-0 sm:text-lg md:text-xl xl:text-3xl ${contactUsStyle["header"]}`}>
+       <div className={"mx-auto sm:px-4"}>
+         <div className={"flex flex-wrap mb-24"}>
+          <div className={`container mx-autosm:px-4 ${contactUsStyle["contact-form"]}`}>
+            <p className={` pt-5 pb-4 sm:text-lg md:text-xl  xl:text-3xl ${contactUsStyle["header"]}`}>
                 Got a question?
             </p>
           <form className={`ml-10 mr-10 ${contactUsStyle["form-input-fields"]}`}>
-              <label htmlFor="first-name">First name</label>
-                  <input className={"h-12 p-3"} type="text" id="firstName" name="firstName" placeholder="your first name..." required />
+            <div className={contactUsStyle["fullname-container"]}>
+             <label htmlFor="first-name">First name</label>
+               <input className={"h-12 p-3"} type="text" id="firstName" name="firstName" placeholder="your first name..." required />
              <label htmlFor="last-name">Last name</label>
-                  <input className={"h-12 p-3"} type="text" id="lastName" name="lastName" placeholder="your last name..." required />
+               <input className={"h-12 p-3"} type="text" id="lastName" name="lastName" placeholder="your last name..." required />
+            </div>
              <label htmlFor="email">Email</label>
-                  <input className={"h-12 p-3"} type="text" id="email" name="email" placeholder="your email..." required />
+               <input className={"h-12 p-3"} type="text" id="email" name="email" placeholder="your email..." required />
              <label htmlFor="message">Your message</label>
-                  <input className={`h-32 p-3 ${contactUsStyle["message-box"]}`} type="text" id="message" name="message" placeholder="type your message here..." required />
+               <input className={`h-32 p-3 ${contactUsStyle["message-box"]}`} type="text" id="message" name="message" placeholder="type your message here..." required />
            </form>
             <div className={contactUsStyle["submit-button"]}>
               <Button
@@ -41,11 +43,12 @@ const ContactUs = () => {
                   cSass={submitButton.cSass}
                   buttontext={submitButton.buttontext}
                 />
-            </div>
-          </div>
+             </div>
          </div>
+       </div>
       </div>
-     <Footer />
+      <GoHomeContainer />
+      <Footer />
     </div>
     )
 }
