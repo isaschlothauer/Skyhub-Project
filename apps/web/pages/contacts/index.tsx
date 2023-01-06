b51ab9338b28e5e291fd215fbd6e86aa30e5dce9
Kukuruzka1 <irinakukuruza295@gmail.com>
2023-01-06 15:10:46 +0100

    modifed contact form to add some padding and adjusted input fileds

--- apps/web/pages/contacts/index.tsx
+++ apps/web/pages/contacts/index.tsx
@@ -22,20 +22,20 @@ const ContactUs = () => {
        <div className={"mx-auto sm:px-4"}>
          <div className={"flex flex-wrap mb-24"}>
           <div className={`container mx-autosm:px-4 ${contactUsStyle["contact-form"]}`}>
-            <p className={` pt-5 pb-4 sm:text-lg md:text-xl  xl:text-3xl ${contactUsStyle["header"]}`}>
+            <p className={`my-12 sm:text-lg md:text-xl  xl:text-3xl ${contactUsStyle["header"]}`}>
                 Got a question?
             </p>
-          <form className={`ml-10 mr-10 ${contactUsStyle["form-input-fields"]}`}>
+          <form className={`ml-10 mr-10 md:px-16 ${contactUsStyle["form-input-fields"]}`}>
             <div className={contactUsStyle["fullname-container"]}>
              <label htmlFor="first-name">First name</label>
-               <input className={"h-12 p-3"} type="text" id="firstName" name="firstName" placeholder="your first name..." required />
+               <input className={"h-12 p-8"} type="textarea" id="firstName" name="firstName" placeholder="your first name..." required />
              <label htmlFor="last-name">Last name</label>
-               <input className={"h-12 p-3"} type="text" id="lastName" name="lastName" placeholder="your last name..." required />
+               <input className={"h-12 p-8"} type="textarea" id="lastName" name="lastName" placeholder="your last name..." required />
             </div>
              <label htmlFor="email">Email</label>
-               <input className={"h-12 p-3"} type="text" id="email" name="email" placeholder="your email..." required />
+               <input className={"h-12 p-8"} type="textarea" id="email" name="email" placeholder="your email..." required />
              <label htmlFor="message">Your message</label>
-               <input className={`h-32 p-3 ${contactUsStyle["message-box"]}`} type="text" id="message" name="message" placeholder="type your message here..." required />
+               <textarea className={`h-32 p-8 ${contactUsStyle["message-box"]}`} id="message" name="message" placeholder="type your message here..." required />
            </form>
             <div className={contactUsStyle["submit-button"]}>
               <Button
