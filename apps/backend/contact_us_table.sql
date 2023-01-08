a424e979666212417b61c8d78e101c41e7b29c1d
Kukuruzka1 <irinakukuruza295@gmail.com>
2023-01-08 20:14:21 +0100

    created post route in backend for submitting contact form. Need to install body-parser package for it

---
+++ apps/backend/contact_us_table.sql
@@ -0,0 +1,11 @@
+-- table for contact us page
+
+CREATE TABLE
+    `contact_us` (
+        `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `first_name` varchar(128) NOT NULL,
+        `last_name` varchar(128) NOT NULL,
+        `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+        `message` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+        `time_submitted` timestamp DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
