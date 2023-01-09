0427d5300705e90e5cbfc1e0ad70ae33504fa59e
Kukuruzka1 <irinakukuruza295@gmail.com>
2023-01-09 14:52:57 +0100

    created axios post request to submit contact form

--- apps/web/components/GeneralButton.tsx
+++ apps/web/components/GeneralButton.tsx
@@ -1,16 +1,22 @@
-import * as React from "react";
+import { HTMLAttributes } from "react";
 import Link from "next/link";
 
-export interface ButtonProps {
+export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   route: string;
   cSass?: string;
   buttontext: string;
 }
 
-const ButtonCMP = ({ cSass, route, buttontext }: ButtonProps) => {
+const ButtonCMP = ({
+  cSass,
+  route,
+  buttontext,
+  className,
+  ...props
+}: ButtonProps) => {
   return (
     <Link href={route}>
-      <button className={`${cSass}`}>
+      <button className={`${className} ${cSass}`.trim()} {...props}>
         {buttontext}
       </button>
     </Link>
