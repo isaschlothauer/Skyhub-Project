f9c438171e8c70aa8226cfcc2b4992fbc1ee6e9b
Kukuruzka1 <irinakukuruza295@gmail.com>
2023-01-09 14:51:47 +0100

    created axios post request for front end to submit contact form. Removed body parser package

--- apps/backend/src/pages/api/router.ts
+++ apps/backend/src/pages/api/router.ts
@@ -1,11 +1,7 @@
 import { Router } from "express";
 import { submitContactForm } from "./handlers";
 
-const bodyParser = require('body-parser'); // needed to parse incoming request body type through json object
 const ContactFormRouter = Router();
-
-ContactFormRouter.use(bodyParser.json()); // for parsing application/json
-ContactFormRouter.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //post method for submitting contact form
 ContactFormRouter.post("/", submitContactForm);
