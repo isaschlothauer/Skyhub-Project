ac39ad9d425e2ca0a23aeec1892b7ab0595be3c9
Isamu Schlothauer <isamu.schlothauer@gmail.com>
2023-01-17 11:28:58 +0100

    Adjusted header for Login, Registration and Password reset page

--- apps/web/pages/login/index.tsx
+++ apps/web/pages/login/index.tsx
@@ -59,82 +59,82 @@ const Login = ({ domain }: LoginProps) => {
   return (
     <div className={styles["login-page"]}>
       {/* Temporary text color change. Revise text color for mobile design */}
-      {/* <div className={"md:text-neutral-50 "}> */}
-        <Mini_Header title={"Sign in"} Scssdomain={domain} />
-      {/* </div> */}
+      <Mini_Header title={"Sign in"} Scssdomain={domain} />
 
-      <div className={`container mx-auto px-2 ${styles["page"]}`}>
-        <div className={"flex flex-wrap"}>
-          <div className={"mx-auto max-w-xl w-full z-10"}>
-            <div
-              className={`pt-7  pb-7 px-4 bg-white shadow-main rounded-[24px]`}
-            >
-              <form className={`${styles["login-input"]} `}>
-                {/* Username input field */}
-                <label htmlFor="username" className={"block text-pink-primary"}>
-                  Username
-                </label>
-                <input
-                  type="text"
-                  className={"border-2 mt-1 w-full rounded-3xl pl-3 h-9"}
-                  name="username"
-                  id="username"
-                  placeholder="Enter username or email address"
-                  value={login.username}
-                  onChange={loginHandler}
-                  required
-                />
-
-                {/* Password input field */}
-                <label
-                  htmlFor="password"
-                  className={"block mt-4 text-pink-primary"}
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className={"border-2 mt-1 w-full rounded-3xl pl-3 h-9"}
-                  name="password"
-                  id="password"
-                  placeholder="Enter password"
-                  value={login.password}
-                  onChange={loginHandler}
-                  required
-                />
-                <div className={"mt-3 flex"}>
+      <div className={"relative top-[220px] md:top-[300px] z-10"}>
+        <div className={`container mx-auto px-2 ${styles["page"]} `}>
+          <div className={"flex flex-wrap"}>
+            <div className={"mx-auto max-w-xl w-full z-10"}>
+              <div
+                className={`pt-7  pb-7 px-4 bg-white shadow-main rounded-[24px]`}
+              >
+                <form className={`${styles["login-input"]} `}>
+                  {/* Username input field */}
+                  <label htmlFor="username" className={"block text-pink-primary"}>
+                    Username
+                  </label>
                   <input
-                    type="checkbox"
-                    checked={remember}
-                    onChange={() => setRemember(!remember)}
-                    className={"ml-3 z-10"}
+                    type="text"
+                    className={"border-2 mt-1 w-full rounded-3xl pl-3 h-9"}
+                    name="username"
+                    id="username"
+                    placeholder="Enter username or email address"
+                    value={login.username}
+                    onChange={loginHandler}
+                    required
                   />
-                  <span className={`ml-2 text-pink-primary`}>Remember me</span>
-                </div>
-              </form>
 
-              {/* Login Submission button */}
-              <div className={"w-min mx-auto mt-4"}>
-                {/* TO DO: Implement authentication process */}
-                <LoginButton
-                  onClick={submitBehavior}
-                  route={loginButton.route}
-                  cSass={loginButton.cSass}
-                  buttontext={loginButton.buttontext}
-                />
-              </div>
-              <div className={`mx-auto w-max mt-3`}>
-                {/* TO DO: Connect link to recovery page */}
-                <Link href="/password_reset" className={`text-pink-primary`}>
-                  Forgot password?
-                </Link>
+                  {/* Password input field */}
+                  <label
+                    htmlFor="password"
+                    className={"block mt-4 text-pink-primary"}
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className={"border-2 mt-1 w-full rounded-3xl pl-3 h-9"}
+                    name="password"
+                    id="password"
+                    placeholder="Enter password"
+                    value={login.password}
+                    onChange={loginHandler}
+                    required
+                  />
+                  <div className={"mt-3 flex"}>
+                    <input
+                      type="checkbox"
+                      checked={remember}
+                      onChange={() => setRemember(!remember)}
+                      className={"ml-3 z-10"}
+                    />
+                    <span className={`ml-2 text-pink-primary`}>Remember me</span>
+                  </div>
+                </form>
+
+                {/* Login Submission button */}
+                <div className={"w-min mx-auto mt-4"}>
+                  {/* TO DO: Implement authentication process */}
+                  <LoginButton
+                    onClick={submitBehavior}
+                    route={loginButton.route}
+                    cSass={loginButton.cSass}
+                    buttontext={loginButton.buttontext}
+                  />
+                </div>
+                <div className={`mx-auto w-max mt-3`}>
+                  {/* TO DO: Connect link to recovery page */}
+                  <Link href="/password_reset" className={`text-pink-primary`}>
+                    Forgot password?
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={"sticky sm:fixed sm:left-0 sm:bottom-0 sm:right-0"}>
-        <Footer />
+        <div className={"fixed left-0 bottom-0 right-0"}>
+          <Footer />
+        </div>
       </div>
     </div>
   );
--- apps/web/pages/registration/index.tsx
+++ apps/web/pages/registration/index.tsx
@@ -127,39 +127,35 @@ const Registration = ({ domain }: RegistrationProps) => {
   }
 
   return (
-    <div className={`${styles["registration-page"]}`}>
-      {/* Temporary text color change. Wait for header component to be fixed. Revise text color for mobile design */}
-      <div className={`md:text-neutral-50`}>
-        <Mini_Header title={"Account Registration"} Scssdomain={domain} />
-      </div>
-
+    <div className={`${styles["registration-page"]} flow-root`}>
+      <Mini_Header title={"Account Registration"} Scssdomain={domain} />
+      
       {/* Account data fields */}
-      <div
-        className={`container bg-white pt-7 px-8 mx-auto rounded-3xl py-3 shadow-main mb-10 md:max-w-xl`}
-      >
-        <div className={"mt-3"}>
-          <p className={"text-center"}>Account type</p>
-          <p className={"mt-5"} >* All fields required</p>
-        <input
-            type="checkbox"
-            checked={airlineRep}
-            onChange={airlineHandler}
-            className={"ml-3 z-10 mt-8"}
-          />
-          <span className={"ml-2"}>Airline representative</span>
-
-          <div className={"block mb-5"}>
+      <div className={"relative top-[260px] md:top-[300px] z-10"}>
+        <div className={`container bg-white pt-7 px-8 mx-auto rounded-3xl py-3 shadow-main mb-10 md:max-w-xl max-w-[600px]`}>
+          <div className={"mt-3"}>
+            <p className={"text-center"}>Account type</p>
+            <p className={"mt-5"} >* All fields required</p>
             <input
               type="checkbox"
-              checked={recruitmentRep}
-              onChange={recruitmentHandler}
-              className={"ml-3 z-10 mt-5"}
+              checked={airlineRep}
+              onChange={airlineHandler}
+              className={"ml-3 z-10 mt-8"}
             />
-            <span className={"ml-2"}>Rectruitment agency</span>
+            <span className={"ml-2"}>Airline representative</span>
+
+            <div className={"block mb-5"}>
+              <input
+                type="checkbox"
+                checked={recruitmentRep}
+                onChange={recruitmentHandler}
+                className={"ml-3 z-10 mt-5"}
+              />
+              <span className={"ml-2"}>Rectruitment agency</span>
+            </div>
           </div>
-        </div>
-        <div className={"text-center mt-8"}>New account data</div>
-          
+          <div className={"text-center mt-8"}>New account data</div>
+            
           {/* Data Input Field */}
           <form>
             <label htmlFor="firstname" className={labelStyling}>Firstname
@@ -249,25 +245,26 @@ const Registration = ({ domain }: RegistrationProps) => {
 
           </form>          
 
-            <div className={"mt-5 mx-2 "}>Upon submission, verification email will be sent to the email address specified. Please follow the link to complete the registration.</div>
-          
-            {/* TOS checkbox */}
-            <input
-              type="checkbox"
-              checked={tos}
-              className={"ml-3 z-10 mt-5"}
-              onChange={(event) => setTOS(event.target.checked)}
-            />
-            <span className={"ml-2 text-pink-primary"}>Agree to <a href="{{ url('terms-of-service') }}" className={"underline"}>the Terms of Service</a></span>
+          <div className={"mt-5 mx-2 "}>Upon submission, verification email will be sent to the email address specified. Please follow the link to complete the registration.</div>
             
-            {/* Account data submission button */}
-            {passwordMatch()}
+          {/* TOS checkbox */}
+          <input
+            type="checkbox"
+            checked={tos}
+            className={"ml-3 z-10 mt-5"}
+            onChange={(event) => setTOS(event.target.checked)}
+          />
+          <span className={"ml-2 text-pink-primary"}>Agree to <a href="{{ url('terms-of-service') }}" className={"underline"}>the Terms of Service</a></span>
+          
+          {/* Account data submission button */}
+          {passwordMatch()}
         </div>
 
-        {/* <ReturnHomeContainer /> */}
+          {/* <ReturnHomeContainer /> */}
 
         <div className={"mt-10"}>      
-        <Footer />
+          <Footer />
+        </div>
       </div>
     </div>
   );
