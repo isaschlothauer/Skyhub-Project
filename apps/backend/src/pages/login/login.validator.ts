33584c693d41bc1c8707c8ac6883aca12cc9084d
Isamu Schlothauer <isamu.schlothauer@gmail.com>
2023-01-20 13:39:11 +0100

    Login function routing have been changed, validator, and hashing added

--- apps/backend/src/pages/login/login.validator.ts
+++ apps/backend/src/pages/login/login.validator.ts
@@ -1,19 +1,22 @@
-import express, { RequestHandler } from "express";
-import { Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
+
+
+interface loginInputData  {
+  email: string;
+  password: string;
+}
 
 export const loginValidator = [
   body("email").not().isEmpty().trim().isEmail(),
   body("password").not().isEmpty().trim().escape(),
-  (req: Request, res: Response, next: NextFunction) => {
+  (req, res, next) => {
     const errors = validationResult(req);
     // console.log(req.body)
 
     if (!errors.isEmpty()) {
       res.status(422).json({ validationErrors: errors.array() });
-    } else if (req.body.password != req.body.passwordRepeat) {
-      return res.status(401).send("Please check your credentials");
     } else {
+      console.log("Vadliation ok");
       next();
     }
   }
--- apps/backend/src/pages/login/loginRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { Login } from "../users/user.handler";
-
-const LoginRouter: Router = Router();
-
-LoginRouter.get("/", Login);
-
-export default LoginRouter;
