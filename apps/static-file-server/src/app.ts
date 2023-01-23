c2471e98f24c221ad55a4ecaaf43e6ea2a779935
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-01-23 11:26:13 +0100

    added static-file-server app for serving public images

---
+++ apps/static-file-server/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT: number = parseInt(process.env.STATIC_PORT ?? "5080");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => console.log("Static file server running on port", PORT));
