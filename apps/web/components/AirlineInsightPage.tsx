83a677adb7042d502bf522511459a63b636aff86
Elif Gömleksiz <e.goemlek@gmail.com>
2023-01-24 14:54:13 +0100

    refactored salary mobile component

--- apps/web/components/AirlineInsightPage.tsx
+++ apps/web/components/AirlineInsightPage.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Mini_Header from "./Header";
 import SalaryMobile from "./SalaryMobile";
-import useMediaQuery from "../hooks/useMediaQuery";
+
 import styles from "./airline_insight_page.module.scss";
 
 export interface GeneralAirlineProps {
@@ -15,7 +15,6 @@ export default function AirlineInsight({
   slug,
   airlineInfo,
 }: GeneralAirlineProps) {
-  const isMobile = useMediaQuery("(max-width: 500px)");
   if (airlineInfo == null) return <p>Loading...</p>;
 
   return (
@@ -33,19 +32,33 @@ export default function AirlineInsight({
           {airlineInfo.name}
         </div>
         <div className="mx-auto container relative z-10 mobile:top-[15.625rem] tablet:top-[16.25rem] pc:top-[20rem]">
-          {isMobile &&
-          (airlineInfo.salary_captain_max ||
-            airlineInfo.salary_captain_avg ||
-            airlineInfo.salary_captain_min) ? (
-            <SalaryMobile
-              jobTitle={"Captain"}
-              maxSalary={airlineInfo.salary_captain_max}
-              avgSalary={airlineInfo.salary_captain_avg}
-              minSalary={airlineInfo.salary_captain_min}
-            />
-          ) : (
-            <></>
-          )}
+          <SalaryMobile
+            jobTitle={"Captain"}
+            maxSalary={airlineInfo.salary_captain_max}
+            avgSalary={airlineInfo.salary_captain_avg}
+            minSalary={airlineInfo.salary_captain_min}
+          />
+
+          <SalaryMobile
+            jobTitle={"Senior First Officer"}
+            maxSalary={airlineInfo.salary_sfo_max}
+            avgSalary={airlineInfo.salary_sfo_avg}
+            minSalary={airlineInfo.salary_sfo_min}
+          />
+
+          <SalaryMobile
+            jobTitle={"First Officer"}
+            maxSalary={airlineInfo.salary_fo_max}
+            avgSalary={airlineInfo.salary_fo_avg}
+            minSalary={airlineInfo.salary_fo_min}
+          />
+
+          <SalaryMobile
+            jobTitle={"Second Officer"}
+            maxSalary={airlineInfo.salary_so_max}
+            avgSalary={airlineInfo.salary_so_avg}
+            minSalary={airlineInfo.salary_so_min}
+          />
         </div>
       </div>
     </div>
--- apps/web/components/SalaryMobile.tsx
+++ apps/web/components/SalaryMobile.tsx
@@ -1,6 +1,6 @@
 import Image, { StaticImageData } from "next/image";
 import SalaryRow from "../components/SalaryRow";
-
+import useMediaQuery from "../hooks/useMediaQuery";
 //images
 import IconAvgSalary from "../assets/images/icons/icon-salary-avg.png";
 import IconMaxSalary from "../assets/images/icons/icon-salary-max.png";
@@ -32,42 +32,49 @@ export default function SalaryMobile({
   avgSalary,
   minSalary,
 }: SalaryMobileProps) {
+  const isMobile = useMediaQuery("(max-width: 500px)");
   return (
-    <div className="flex flex-col justify-center items-stretch rounded-[15px] bg-white shadow-main px-6 pt-3 pb-6 gap-4">
-      <div className="flex flex-row justify-center items-center relative mb-2 mt-1">
-        <Image
-          src={icon || rankIcons[jobTitle]}
-          alt="icon captain"
-          className=" object-contain h-7 w-10"
-        />
-        <h3 className=" text-[#000e94] text-base font-extrabold pl-2 ">
-          {jobTitle}
-        </h3>
-      </div>
-      {maxSalary && (
-        <SalaryRow
-          iconSalary={IconMaxSalary}
-          iconHeight={19}
-          salaryType="Maximum Salary"
-          salaryValue={maxSalary}
-        />
-      )}
-      {avgSalary && (
-        <SalaryRow
-          iconSalary={IconAvgSalary}
-          salaryType="Average Salary"
-          salaryValue={avgSalary}
-        />
-      )}
+    <>
+      {isMobile && (maxSalary || avgSalary || minSalary) ? (
+        <div className="flex flex-col justify-center items-stretch rounded-[15px] bg-white shadow-main px-6 pt-3 pb-6 mb-6 gap-4">
+          <div className="flex flex-row justify-center items-center relative mb-2 mt-1">
+            <Image
+              src={icon || rankIcons[jobTitle]}
+              alt="icon captain"
+              className=" object-contain h-7 w-10"
+            />
+            <h3 className=" text-[#000e94] text-base font-extrabold pl-2 ">
+              {jobTitle}
+            </h3>
+          </div>
+          {maxSalary && (
+            <SalaryRow
+              iconSalary={IconMaxSalary}
+              iconHeight={19}
+              salaryType="Maximum Salary"
+              salaryValue={maxSalary}
+            />
+          )}
+          {avgSalary && (
+            <SalaryRow
+              iconSalary={IconAvgSalary}
+              salaryType="Average Salary"
+              salaryValue={avgSalary}
+            />
+          )}
 
-      {minSalary && (
-        <SalaryRow
-          iconSalary={IconMinSalary}
-          iconHeight={19}
-          salaryType="Minimum Salary"
-          salaryValue={minSalary}
-        />
+          {minSalary && (
+            <SalaryRow
+              iconSalary={IconMinSalary}
+              iconHeight={19}
+              salaryType="Minimum Salary"
+              salaryValue={minSalary}
+            />
+          )}
+        </div>
+      ) : (
+        <></>
       )}
-    </div>
+    </>
   );
 }
